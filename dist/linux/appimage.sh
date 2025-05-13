57b3b964875c5693c88b7880f6b00c52095f85b3
SSimco <37044560+SSimco@users.noreply.github.com>
2025-05-13 08:59:38 +0300

    Update runner & scripts to support aarch64 builds for linux

--- dist/linux/appimage.sh
+++ dist/linux/appimage.sh
@@ -4,18 +4,18 @@ if [[ -z "${GITHUB_WORKSPACE}" ]]; then
 	export GITHUB_WORKSPACE="."
 fi
 
-curl -sSfLO "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
-chmod a+x linuxdeploy*.AppImage
-curl -sSfL https://github.com"$(curl https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous | grep "mkappimage-.*-x86_64.AppImage" | head -n 1 | cut -d '"' -f 2)" -o mkappimage.AppImage
+if [[ -z "${CEMU_ARCH}" ]]; then
+	export CEMU_ARCH="x86_64"
+fi
+
+curl -sSfL "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-$CEMU_ARCH.AppImage" -o linuxdeploy.AppImage
+chmod a+x linuxdeploy.AppImage
+curl -sSfL https://github.com"$(curl https://github.com/probonopd/go-appimage/releases/expanded_assets/continuous | grep "mkappimage-.*-$CEMU_ARCH.AppImage" | head -n 1 | cut -d '"' -f 2)" -o mkappimage.AppImage
 chmod a+x mkappimage.AppImage
 curl -sSfLO "https://raw.githubusercontent.com/linuxdeploy/linuxdeploy-plugin-gtk/master/linuxdeploy-plugin-gtk.sh"
 chmod a+x linuxdeploy-plugin-gtk.sh
 curl -sSfLO "https://github.com/darealshinji/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt.sh"
 chmod a+x linuxdeploy-plugin-checkrt.sh
-
-if [[ ! -e /usr/lib/x86_64-linux-gnu ]]; then
-	sed -i 's#lib\/x86_64-linux-gnu#lib64#g' linuxdeploy-plugin-gtk.sh
-fi
 
 mkdir -p AppDir/usr/bin
 mkdir -p AppDir/usr/share/Cemu
@@ -32,11 +32,33 @@ cp -r bin/* AppDir/usr/share/Cemu
 mv AppDir/usr/share/Cemu/Cemu AppDir/usr/bin/
 chmod +x AppDir/usr/bin/Cemu
 
-cp /usr/lib/x86_64-linux-gnu/{libsepol.so.1,libffi.so.7,libpcre.so.3,libGLU.so.1,libthai.so.0} AppDir/usr/lib
+libs=(
+	libsepol.so
+	libffi.so
+	libpcre.so
+	libGLU.so
+	libthai.so
+)
+
+for lib in "${libs[@]}"; do
+	libpath="/usr/lib/$CEMU_ARCH-linux-gnu/$lib"
+
+	if [ ! -e "$libpath" ]; then
+		libpath="/usr/lib/$lib"
+		if [ ! -e "$libpath" ]; then
+			echo "Library $lib not found at /usr/lib/$CEMU_ARCH-linux-gnu/$lib or /usr/lib/$lib, skipping."
+			continue
+		fi
+	fi
+
+	libpath=$(realpath "$libpath")
+
+	cp "$libpath" AppDir/usr/lib
+done
 
 export UPD_INFO="gh-releases-zsync|cemu-project|Cemu|ci|Cemu.AppImage.zsync"
 export NO_STRIP=1
-./linuxdeploy-x86_64.AppImage --appimage-extract-and-run \
+./linuxdeploy.AppImage --appimage-extract-and-run \
   --appdir="${GITHUB_WORKSPACE}"/AppDir/ \
   -d "${GITHUB_WORKSPACE}"/AppDir/info.cemu.Cemu.desktop \
   -i "${GITHUB_WORKSPACE}"/AppDir/info.cemu.Cemu.png \
@@ -49,9 +71,9 @@ if ! GITVERSION="$(git rev-parse --short HEAD 2>/dev/null)"; then
 fi
 echo "Cemu Version Cemu-${GITVERSION}"
 
-rm AppDir/usr/lib/libwayland-client.so.0
+rm AppDir/usr/lib/libwayland-client.so.* || true
 echo -e "export LC_ALL=C\nexport FONTCONFIG_PATH=/etc/fonts" >> AppDir/apprun-hooks/linuxdeploy-plugin-gtk.sh
 VERSION="${GITVERSION}" ./mkappimage.AppImage --appimage-extract-and-run "${GITHUB_WORKSPACE}"/AppDir
 
 mkdir -p "${GITHUB_WORKSPACE}"/artifacts/
-mv Cemu-"${GITVERSION}"-x86_64.AppImage "${GITHUB_WORKSPACE}"/artifacts/
+mv Cemu-"${GITVERSION}"-$CEMU_ARCH.AppImage "${GITHUB_WORKSPACE}"/artifacts/
